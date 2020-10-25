5041a56a7672a9e7565b6acd6b42f392fd26bfed
paschtet <pavel.ak@icloud.com>
2020-10-25 15:11:22 +0600

    Добавил пробелов для упрощения читания кода

--- cycle.pas
+++ cycle.pas
@@ -4,10 +4,13 @@ var
     j:integer;
 
 begin
+
    writeln('Введите начала цикла');
    readln(a);
+
    writeln('Введите конец цикла');
    readln(b);
+
    if a<b then begin
       writeln('FOR');
       for j:=a to b do
